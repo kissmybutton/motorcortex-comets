9894c44e2e584e9bfe33691e257b7f3263b98d93
Andreas Trantidis <atrantidis@gmail.com>
2021-02-18 22:16:19 +0000

    spatial

--- src/helpers/randomizer.js
+++ src/helpers/randomizer.js
@@ -1,43 +1,67 @@
+function _shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
 /*
 params: {
     from,
     to,
     numberOfElements,
-    divisions,
-    points
+    divisions
 }
 */
-function _produceRandomSpacialForOneDimension(params){
-    const points = [];
-
+function _addDimension(params, points) {
+    if(points !== null){
+        points = _shuffle(points);
+    } else{
+        for(let i=0; i<params.numberOfElements; i++){
+            points.push([]);
+        }
+    }
+    
     const lastRowElements = params.numberOfElements % params.divisions;
     const fullRows = (params.numberOfElements - lastRowElements) / params.divisions;
     const step = params.to - params.from / divisions;
 
     const divisionsByElement = [];
-    for(let row=0; row<fullRows; row++){
-        for(let i=0; i<params.divisions; i++){ // for each division
+    for (let row = 0; row < fullRows; row++) {
+        for (let i = 0; i < params.divisions; i++) { // for each division
             const limits = {
-                from: params.from + step*i,
+                from: params.from + step * i,
                 to: params.from + step * (i + 1)
             }
             divisionsByElement.push(limits);
         }
     }
-    
-    if(lastRowElements !== 0){
+
+    if (lastRowElements !== 0) {
         const lastRowStep = params.to - params.from / lastRowElements;
-        for(let i=0; i<lastRowElements; i++){
+        for (let i = 0; i < lastRowElements; i++) {
             const limits = {
-                from: params.from + lastRowStep*i,
+                from: params.from + lastRowStep * i,
                 to: params.from + lastRowStep * (i + 1)
             }
             divisionsByElement.push(limits);
         }
     }
-    
-    for(let i=0; i<divisionsByElement.length; i++){
-        points.push(Math.random() * (divisionsByElement[i].to - divisionsByElement[i].from) + divisionsByElement[i].from);
+
+    for (let i = 0; i < divisionsByElement.length; i++) {
+        points[i].push(Math.random() * (divisionsByElement[i].to - divisionsByElement[i].from) + divisionsByElement[i].from);
     }
 
     return points;
@@ -55,21 +79,24 @@ params: {
     nunmberOfElements: <integer>  
 }
 */
-export function spatial(params){
-    const points = [];
-    for(let i=0; i<params.dimensions.length; i++){
+export function spatial(params) {
+    let points = null;
+    for (let i = 0; i < params.dimensions.length; i++) {
         const limits = params.dimensions[i];
         const divisions = params.divisions[i] || 4;
-        const points = _produceRandomSpacialForOneDimension({
-            from: limits[0],
-            to: limits[1],
-            numberOfElements: params.numberOfElements,
-            divisions: divisions
-        });
+        points = _addDimension(
+            {
+                from: limits[0],
+                to: limits[1],
+                numberOfElements: params.numberOfElements,
+                divisions: divisions
+            }, 
+            points
+        );
     }
-    // get dimension by dimension and produce points
+    return points;
 }
 
-export function timely(params){
+export function timely(params) {
 
 }
