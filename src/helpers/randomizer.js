5e7de9f001b0f1e666aee68f222dfe4f040b03a5
Andreas Trantidis <atrantidis@gmail.com>
2021-02-19 13:01:11 +0000

    timely version

--- src/helpers/randomizer.js
+++ src/helpers/randomizer.js
@@ -1,4 +1,6 @@
 const defaultDivisions = 4;
+const minDurationDefault = 0.1;
+const maxDurationDefault = 0.6;
 
 function _shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -53,7 +55,7 @@ function _addDimension(params, points) {
     }
 
     if (lastRowElements !== 0) {
-        const lastRowStep = params.to - params.from / lastRowElements;
+        const lastRowStep = (params.to - params.from) / lastRowElements;
         for (let i = 0; i < lastRowElements; i++) {
             const limits = {
                 from: params.from + lastRowStep * i,
@@ -124,5 +126,36 @@ export function timely(params) {
         , null
     );
     
+    startAndDuration = _addDimension({
+        numberOfElements: params.numberOfElements,
+        from: 0.3,
+        to: 1,
+        divisions: params.divisions || defaultDivisions
+    }, startAndDuration);
 
+    // now we are going to calculate the exact duration of each element
+    // and replace the fraction that we have produced on the previous step
+    
+    // a variable to be used for making sure we make the last element's duration equal the number needed
+    // so the full sequence has the desired duration
+    let closestToEnd = {
+        ms: 0,
+        reference: null
+    };
+
+    for(let i=0; i<startAndDuration.length; i++){
+        const element = startAndDuration[i];
+        element[0] = Math.round(element[0]);
+        if(closestToEnd.ms < element[0]){
+            closestToEnd = {
+                ms: element[0],
+                reference: element
+            }
+        }
+        element[1] = Math.floor((params.duration - element[0]) < params.duration * (params.maxDuration || maxDurationDefault) ? (params.duration - element[0]) * element[1]
+            : (params.maxDuration || maxDurationDefault) * params.duration * element[1]);
+    }
+    closestToEnd.reference[1] = params.duration - closestToEnd.reference[0];
+
+    return startAndDuration;
 }
